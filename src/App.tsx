b7dda10e27895b9c4c9c8cc5caed6df840f0f7f8
laxman1129 <laxman1129@gmail.com>
2020-06-02 23:24:29 +0400

    Welcome
    Added readme file and updated text on the page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello, World!!!</h1>
+      <h2>Welcome to the first react application.</h2>
     </div>
   );
 }
